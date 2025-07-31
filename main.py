287f70a80c3044ca7f615250c2b3b1a4dff14d16
Vhercules <vhercules@correo.unimet.edu.ve>
2025-07-30 21:22:47 -0400

    menu

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("creando el repositorio")
-print("probando todo este bien")
+def mostrar_menu():
+    """
+    METROART
+    """
+    print("\n--- Catálogo---")
+    print("1. Buscar obras por Departamento")
+    print("2. Buscar obras por Nacionalidad del Autor")
+    print("3. Buscar obras por Nombre del Autor")
+    print("4. Mostrar detalles de una obra por ID")
+    print("5. Salir")
+    print("--------------------------")
+
+
+main()
