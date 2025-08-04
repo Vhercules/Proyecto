492d3645eff4f7d254c54f9b75310258f96b6e4a
Vhercules <vhercules@correo.unimet.edu.ve>
2025-08-04 00:53:43 -0400

    creando las clases iniciales

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+class ObraDeArte:
+    def __init__(self, id, titulo, nombre_artista, nacionalidad_artista,
+                 fecha_nacimiento_artista, fecha_muerte_artista,
+                 tipo, anio_creacion, url_imagen, departamento):
+        self.id = id
+        self.titulo = titulo
+        self.nombre_artista = nombre_artista
+        self.nacionalidad_artista = nacionalidad_artista
+        self.fecha_nacimiento_artista = fecha_nacimiento_artista
+        self.fecha_muerte_artista = fecha_muerte_artista
+        self.tipo = tipo
+        self.anio_creacion = anio_creacion
+        self.url_imagen = url_imagen
+        self.departamento = departamento
+
+    def __str__(self):
+        return (f"ID: {self.id}\n"
+                f"Título: {self.titulo}\n"
+                f"Artista: {self.nombre_artista} ({self.nacionalidad_artista})\n"
+                f"Departamento: {self.departamento}")
+
+    def mostrar_detalles(self):
+        print("\n--- Detalles de la Obra ---")
+        print(f"ID: {self.id}")
+        print(f"Título: {self.titulo}")
+        print(f"Artista: {self.nombre_artista}")
+        print(f"  Nacionalidad: {self.nacionalidad_artista}")
+        print(f"  Fecha de nacimiento: {self.fecha_nacimiento_artista}")
+        print(f"  Fecha de muerte: {self.fecha_muerte_artista}")
+        print(f"Departamento: {self.departamento}")
+        print(f"Tipo: {self.tipo}")
+        print(f"Año de creación: {self.anio_creacion}")
+        print(f"URL de la imagen: {self.url_imagen}")
+        print("--------------------------")
+
+class Departamento:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+
+    def __str__(self):
+        return f"ID: {self.id} - Nombre: {self.nombre}"
+
 def mostrar_menu():
     """
     METROART
@@ -11,4 +54,4 @@ def mostrar_menu():
     print("--------------------------")
 
 
-main()
+mostrar_menu()
